07df4d4f7d231dbeb15ad58b12979faa29137108
rayansb1 <rayanbawazeer1@hotmail.com>
2024-10-11 16:45:16 +0300

    Finish home page 💻

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,2 +1,72 @@
-// We will create a page that it the main page for the app,
-// and it will have to button to bring you to section you need it (notification, timer).
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 232, 232, 232),
+        body: Center(
+          child: Column(
+            mainAxisAlignment:
+                MainAxisAlignment.center, // Centers the content vertically
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 50), // Moves text a little up
+                child: Text(
+                  "Welcome to our App 👋🏼",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centers buttons horizontally
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigate to the notification page
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      textStyle: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: const Text('Notifications'),
+                  ),
+                  const SizedBox(
+                      width: 20), // Adds space between the two buttons
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigate to the timer page
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.green,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      textStyle: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: const Text('Timer'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
